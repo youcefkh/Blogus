5676b23274fdb1beda1c9737909e2624ed623d60
youcefkh <58133286+youcefkh@users.noreply.github.com>
2023-10-11 13:47:45 +0100

    implement pagination for posts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,10 @@
         @endforeach
 
         <!-- Pagination -->
-        <div class="flex items-center py-8">
+        <!-- views/vendor/pagination/tailwind.blade.php -->
+        {{$posts->links()}}
+
+        {{-- <div class="flex items-center py-8">
             <a href="#"
                 class="h-10 w-10 bg-blue-800 hover:bg-blue-600 font-semibold text-white text-sm flex items-center justify-center">1</a>
             <a href="#"
@@ -17,7 +20,7 @@
             <a href="#"
                 class="h-10 w-10 font-semibold text-gray-800 hover:text-gray-900 text-sm flex items-center justify-center ml-3">Next
                 <i class="fas fa-arrow-right ml-2"></i></a>
-        </div>
+        </div> --}}
 
     </section>
 
