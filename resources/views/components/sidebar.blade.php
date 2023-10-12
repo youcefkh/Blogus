f449f619363a9e5b75bc8b4f1f02253de168728b
youcefkh <58133286+youcefkh@users.noreply.github.com>
2023-10-12 15:14:41 +0100

    set redirection for about-us page on sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -11,7 +11,7 @@
     <div class="w-full bg-white shadow flex flex-col my-4 p-6">
         <p class="text-xl font-semibold pb-5">{{ \App\Models\TextWidget::getTitle('about-us') }}</p>
         <p class="pb-2">{!! \App\Models\TextWidget::getContent('about-us') !!}</p>
-        <a href="#"
+        <a href="{{ route('about-us') }}"
             class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-4">
             Get to know us
         </a>
