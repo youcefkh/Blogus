205c8aef80e85c6fa0e6bedaa3c8857adf2e56cd
youcefkh <58133286+youcefkh@users.noreply.github.com>
2023-10-17 18:06:24 +0100

    implement recommended posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -23,31 +23,58 @@ class PostController extends Controller
     {
         //Latest posts
         $latestPosts = Post::where('active', 1)
-                    ->where('published_at', '<=', Carbon::now())
-                    ->orderBy('published_at', 'desc')
-                    ->limit(8)
-                    ->get();
+            ->where('published_at', '<=', Carbon::now())
+            ->orderBy('published_at', 'desc')
+            ->limit(8)
+            ->get();
 
         //show most popular 3 posts based on upvotes
         $popularPosts = Post::leftJoin('post_votes', 'posts.id', '=', 'post_votes.post_id')
-                            ->select('posts.*', DB::raw('COUNT(post_votes.upvote) as likes'))
-                            ->where(function($query){
-                                $query->whereNull('post_votes.upvote')
-                                    ->orWhere('post_votes.upvote', 1);
-                            })
-                            ->where('active', 1)
-                            ->where('published_at', '<=', Carbon::now())
-                            ->groupBy('posts.id')
-                            ->orderBy('likes', 'desc')
-                            ->limit(3)
-                            ->get();
+            ->select('posts.*', DB::raw('COUNT(post_votes.upvote) as likes'))
+            ->where(function ($query) {
+                $query->whereNull('post_votes.upvote')
+                    ->orWhere('post_votes.upvote', 1);
+            })
+            ->where('active', 1)
+            ->where('published_at', '<=', Carbon::now())
+            ->groupBy('posts.id')
+            ->orderBy('likes', 'desc')
+            ->limit(3)
+            ->get();
 
         //if authorized - show recommended posts based on upvotes
         //if not - popular posts based on views
+        $user = auth()->user();
+        if ($user) {
+            //select posts from the same category the user has upvoted and exclude the posts he actually upvoted
+            $leftJoin = "(SELECT cp.category_id, cp.post_id FROM post_votes JOIN category_post cp
+                        ON cp.post_id = post_votes.post_id WHERE post_votes.upvote = 1 AND post_votes.user_id = $user->id) as t";
+
+            $recommendedPosts = Post::leftJoin('category_post as cp', 'posts.id', '=', 'cp.post_id')
+                ->leftJoin(DB::raw($leftJoin), function ($join) {
+                    $join->on('cp.category_id', '=', 't.category_id');
+                })
+                ->select('posts.*')
+                ->where('cp.post_id', '<>', DB::raw('t.post_id'))
+                ->where('active', 1)
+                ->where('published_at', '<=', Carbon::now())
+                ->groupBy('posts.id')
+                ->limit(6)
+                ->get();
+        } else {
+            $recommendedPosts = Post::leftJoin('post_views', 'posts.id', '=', 'post_views.post_id')
+                ->select('posts.*', DB::raw('COUNT(post_views.id) as views_count'))
+                ->where('active', 1)
+                ->where('published_at', '<=', Carbon::now())
+                ->groupBy('posts.id')
+                ->orderBy('views_count', 'desc')
+                ->limit(3)
+                ->get();
+        }
 
         //show categories with their latest posts
-        
-        return view('home', compact('latestPosts', 'popularPosts'));
+
+        return view('home', compact('latestPosts', 'popularPosts', 'recommendedPosts'));
     }
 
     /**
@@ -55,25 +82,25 @@ class PostController extends Controller
      */
     public function show(Post $post, Request $request)
     {
-        if(!$post->active || $post->published_at > Carbon::now()) {
+        if (!$post->active || $post->published_at > Carbon::now()) {
             abort(404);
         }
 
         $prev = Post::where('active', 1)
-                    ->where('published_at', '<=', Carbon::now())
-                    ->where('published_at', '>', $post->published_at)
-                    ->select('slug', 'title')
-                    ->orderBy('published_at', 'asc')
-                    ->limit(1)
-                    ->first();
+            ->where('published_at', '<=', Carbon::now())
+            ->where('published_at', '>', $post->published_at)
+            ->select('slug', 'title')
+            ->orderBy('published_at', 'asc')
+            ->limit(1)
+            ->first();
 
         $next = Post::where('active', 1)
-                    ->where('published_at', '<=', Carbon::now())
-                    ->where('published_at', '<', $post->published_at)
-                    ->select('slug', 'title')
-                    ->orderBy('published_at', 'desc')
-                    ->limit(1)
-                    ->first();
+            ->where('published_at', '<=', Carbon::now())
+            ->where('published_at', '<', $post->published_at)
+            ->select('slug', 'title')
+            ->orderBy('published_at', 'desc')
+            ->limit(1)
+            ->first();
 
         /** count views */
         $this->countView($request, $post);
@@ -81,20 +108,21 @@ class PostController extends Controller
         return view('post.show', compact('post', 'next', 'prev'));
     }
 
-    public function byCategory (Category $category) {
+    public function byCategory(Category $category)
+    {
         $posts = Post::join('category_post', 'posts.id', '=', 'category_post.post_id')
-                    ->where('category_post.category_id', $category->id)
-                    ->where('active', 1)
-                    ->where('published_at', '<=', Carbon::now())
-                    ->select('posts.*')
-                    ->with(['categories' => function ($query) {
-                        $query->select('slug', 'title');
-                    }])
-                    ->with(['user' => function ($query) {
-                        $query->select('id', 'name');
-                    }])
-                    ->orderBy('published_at', 'desc')
-                    ->paginate(5);
+            ->where('category_post.category_id', $category->id)
+            ->where('active', 1)
+            ->where('published_at', '<=', Carbon::now())
+            ->select('posts.*')
+            ->with(['categories' => function ($query) {
+                $query->select('slug', 'title');
+            }])
+            ->with(['user' => function ($query) {
+                $query->select('id', 'name');
+            }])
+            ->orderBy('published_at', 'desc')
+            ->paginate(5);
 
         return view('category.index', compact('posts', 'category'));
     }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout meta-description="Blogus is a home for human stories and ideas">
 
     <section class="container max-w-5xl mx-auto py-6 px-3">
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-6 mb-8">
             <!-- 3 popular posts -->
             <div class="col-span-2">
                 <h2 class="text-lg sm:text-xl font-bold text-blue-500 uppercase pb-1 border-b-2  border-blue-500 mb-3">
@@ -36,16 +36,33 @@
         </div>
 
         <!-- recommended posts -->
-        <div>
-            <div class="col-span-2">
-                <h2 class="text-lg sm:text-xl font-bold text-blue-500 uppercase pb-1 border-b-2  border-blue-500 mb-3">
-                    Recommended posts
-                </h2>
+        <div class="mb-8">
+            <h2 class="text-lg sm:text-xl font-bold text-blue-500 uppercase pb-1 border-b-2  border-blue-500 mb-3">
+                Recommended posts
+            </h2>
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                @foreach ($recommendedPosts as $post)
+                    <div class="col-pan-1 grid grid-cols-4 gap-2 mb-6">
+                        <a href="{{ route('post.show', $post) }}" class="overflow-hidden">
+                            <img class="rounded h-20 w-full object-cover" src="{{ $post->getThumbnail() }}">
+                        </a>
+                        <div class="col-span-3">
+                            <a href="{{ route('post.show', $post) }}">
+                                <h3 class="font-semibold leading-5">{{ mb_strimwidth($post->title, 0, 50, '...') }}</h3>
+                            </a>
+                            <a href="{{ route('post.show', $post) }}">
+                                <p class="text-sm leading-4">
+                                    {{ mb_strimwidth(strip_tags($post->body), 0, 100, '...') }}
+                                </p>
+                            </a>
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
 
         <!-- recent categories -->
-        <div>
+        <div class="mb-8">
             <div class="col-span-2">
                 <h2 class="text-lg sm:text-xl font-bold text-blue-500 uppercase pb-1 border-b-2  border-blue-500 mb-3">
                     Recent categories
